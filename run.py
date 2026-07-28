fff8506b76a9b94be0e41aec456dcddd60c6e174
Tirumalasetty Bharath chandu <159546807+BharathChandu3@users.noreply.github.com>
2026-07-28 12:04:11 +0530

    Update app run configuration for environment variable

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
+import os
 from speakmate.app import app
 
 if __name__ == "__main__":
-    # Runs the SpeakMate AI app local server
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
